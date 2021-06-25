0bdd223571eb102497504e5b2bd1bd308ed1e88f
Dkuehn <d.keddy.k@googlemail.com>
2021-06-25 07:36:12 +0200

    gitignor + basic server & app setup

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
+// OpenWeather API
+const apiKey = '0dd895f956674403fd2476657ac0604a';
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+/**
+ *  Node and Express Environment
+ */
+const express = require('express');
+const app = express();
 
-// Start up an instance of app
+/**
+ * Project Dependencies
+ */
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+/* Cors */
+const cors = require('cors');
+app.use(cors());
 
-// Initialize the main project folder
+/**
+ * Server Configuration
+ */
+
+// Static link to Website folder
 app.use(express.static('website'));
 
+// Port Setting
+const port = 5000;
 
-// Setup Server
+// Lauchning Port Listener
+app.listen(port, () => log('> Server is up and running on port : ' + port));
+
+/**
+ * Functional Behavior
+ */
+
+/* Routes */
+
+// GET Weather based on a ZIP code
+app.get('/weather', (req, res) => {
+  console.log(req.query);
+  res.send(req.query.zip);
+});
+
+// Protocol Functions
+function log(message) {
+  console.log(`${new Date().toISOString()} - Server Log: ${message}`);
+}
--- website/app.js
+++ website/app.js
@@ -15,3 +15,17 @@ let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 /* Function to POST data */
 
 /* Function to GET Project Data */
+/**
+ * Requests the current weather from the server based on the zip and returns an object containing the weather info
+ *
+ * @param {String} zip Common postal-zip codes which should be handeled by Weather APIs.
+ * @return {Object} representing the weather info provided by the server
+ */
+function getWeather(zip) {
+  return fetch(`/weather?zip=${zip}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json().then((weatherObj) => weatherObj));
+}
